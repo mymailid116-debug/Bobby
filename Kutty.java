2052d36966e5cf04c0a06f492841dc6d2c5080e7
Bobby <mymailid116@gmail.com>
2026-01-04 20:55:59 +0530

    first commit

---
+++ Kutty.java
@@ -0,0 +1,11 @@
+public class Kutty    {
+    public static void main (String[] args){
+
+    System.out.println(" finally its working !! ");
+display();
+
+    }
+    public static void display(){
+        System.out.println(" kutty is here ");
+    }
+}
